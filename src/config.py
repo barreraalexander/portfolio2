1e248130c16ebde0b9c3fd79ae1c884eab0610ad
barreratitleleap <abarrera@titleleap.com>
2023-04-20 10:11:39 -0400

    added requiremnets for cache

--- src/config.py
+++ src/config.py
@@ -2,9 +2,9 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     secret_key: str = 'osmeke'
-    # debugging: bool = True
     debug: bool = True
     port: int = 4000
-    # PORT: int = 4000
+    cache_default_timeout: int = 180
+    cache_type: str = 'SimpleCache'
 
 settings = Settings()
