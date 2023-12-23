8cb461360361c1044b1db93cae5b088aa0e3121e
Alexander Barrera <barreraalexander93@gmail.com>
2023-12-22 19:28:17 -0500

    updated merged and saved

--- run.py
+++ run.py
@@ -1,18 +1,6 @@
-<<<<<<< HEAD
-from src import create_app
-from src.config import settings
-=======
 from website import create_app
->>>>>>> production
 
 app = create_app()
 
 if __name__=='__main__':
-<<<<<<< HEAD
-    app.run(
-        port=settings.port,
-        debug=settings.debug
-    )
-=======
     app.run(debug=True)
->>>>>>> production
